6cb037ebd45f478e3d1c4e3c22c000c0c4d49172
TonyQuesada <Tonyquesadaxd@gmail.com>
2022-01-24 17:06:45 -0600

    a
    a

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
+import Dropdown from "./Dropdown"
 import Footer from "./Footer"
 import Header from "./Header"
 import { GlobalStyle } from "./styles/GlobalStyles"
 
-const Layout = ({ children }) => {
+const Layout = ({ children }) => {  
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <main>{children}</main>
       <Footer />
     </>
